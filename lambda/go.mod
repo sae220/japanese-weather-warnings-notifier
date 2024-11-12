b6bcc5b9f0e861aeb212b96329cc16908cb30707
sae220 <mod-sharp.truetle3@outlook.jp>
2024-11-12 14:37:55 +0900

    Add line bot

--- lambda/go.mod
+++ lambda/go.mod
@@ -3,3 +3,5 @@ module lambda
 go 1.23.2
 
 require github.com/aws/aws-lambda-go v1.47.0
+
+require github.com/line/line-bot-sdk-go/v8 v8.10.0 // indirect
--- lambda/main.go
+++ lambda/main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"os"
+
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/line/line-bot-sdk-go/v8/linebot/messaging_api"
 )
 
-func HandleLambda() (*string, error) {
-	message := "Hello World!"
-	return &message, nil
+func HandleLambda() error {
+	lineBot, err := messaging_api.NewMessagingApiAPI(os.Getenv("LINE_CHANNEL_TOKEN"))
+	if err != nil {
+		return err
+	}
+
+	_, err = lineBot.Broadcast(
+		&messaging_api.BroadcastRequest{
+			Messages: []messaging_api.MessageInterface{
+				messaging_api.TextMessage{
+					Text: "Hello world!",
+				},
+			},
+		},
+		"",
+	)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func main() {
