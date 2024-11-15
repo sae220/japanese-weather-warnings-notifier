eefec90125990aa5fbbf2a76dea530180d974ba8
sae220 <mod-sharp.truetle3@outlook.jp>
2024-11-15 12:45:50 +0900

    Simplify main function

--- lambda/main.go
+++ lambda/main.go
@@ -1,28 +1,16 @@
 package main
 
 import (
-	"os"
-
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/line/line-bot-sdk-go/v8/linebot/messaging_api"
 )
 
 func HandleLambda() error {
-	lineBot, err := messaging_api.NewMessagingApiAPI(os.Getenv("LINE_CHANNEL_TOKEN"))
+	lineBot, err := InitLineBot()
 	if err != nil {
 		return err
 	}
 
-	_, err = lineBot.Broadcast(
-		&messaging_api.BroadcastRequest{
-			Messages: []messaging_api.MessageInterface{
-				messaging_api.TextMessage{
-					Text: "Hello world!",
-				},
-			},
-		},
-		"",
-	)
+	_, err = BroadcastSimpleMessage(lineBot, "Hello World!")
 	if err != nil {
 		return err
 	}
