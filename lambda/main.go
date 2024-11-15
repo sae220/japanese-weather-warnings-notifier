70648657909edf4efe8077ec13e1ca3c47784740
sae220 <mod-sharp.truetle3@outlook.jp>
2024-11-15 12:47:38 +0900

    Update lambda error message

--- lambda/main.go
+++ lambda/main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func HandleLambda() error {
 	lineBot, err := InitLineBot()
 	if err != nil {
-		return err
+		return fmt.Errorf("line bot failed in initialization: %s", err)
 	}
 
 	_, err = BroadcastSimpleMessage(lineBot, "Hello World!")
 	if err != nil {
-		return err
+		return fmt.Errorf("line bot failed in broadcasting: %s", err)
 	}
 	return nil
 }
