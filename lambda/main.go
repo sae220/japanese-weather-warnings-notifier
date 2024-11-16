fbc528d600ae8d73ee558efd9e5b90149adb873f
sae220 <mod-sharp.truetle3@outlook.jp>
2024-11-16 12:54:25 +0900

    Revert main.go

--- lambda/main.go
+++ lambda/main.go
@@ -2,9 +2,28 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/line/line-bot-sdk-go/v8/linebot/messaging_api"
 )
+
+func InitLineBot() (*messaging_api.MessagingApiAPI, error) {
+	return messaging_api.NewMessagingApiAPI(os.Getenv("LINE_CHANNEL_TOKEN"))
+}
+
+func BroadcastSimpleMessage(lineBot *messaging_api.MessagingApiAPI, text string) (*map[string]interface{}, error) {
+	return lineBot.Broadcast(
+		&messaging_api.BroadcastRequest{
+			Messages: []messaging_api.MessageInterface{
+				messaging_api.TextMessage{
+					Text: text,
+				},
+			},
+		},
+		"",
+	)
+}
 
 func HandleLambda() error {
 	lineBot, err := InitLineBot()
