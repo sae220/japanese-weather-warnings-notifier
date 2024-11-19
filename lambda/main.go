39dba97d3daeaa345156c1a45f3b0de72d6305e4
sae220 <mod-sharp.truetle3@outlook.jp>
2024-11-20 08:53:11 +0900

    LINE botを構造体にした

--- lambda/main.go
+++ lambda/main.go
@@ -8,12 +8,20 @@ import (
 	"github.com/line/line-bot-sdk-go/v8/linebot/messaging_api"
 )
 
-func InitLineBot() (*messaging_api.MessagingApiAPI, error) {
-	return messaging_api.NewMessagingApiAPI(os.Getenv("LINE_CHANNEL_TOKEN"))
+type LineBot struct {
+	api *messaging_api.MessagingApiAPI
 }
 
-func BroadcastSimpleMessage(lineBot *messaging_api.MessagingApiAPI, text string) (*map[string]interface{}, error) {
-	return lineBot.Broadcast(
+func InitLineBot() (*LineBot, error) {
+	api, err := messaging_api.NewMessagingApiAPI(os.Getenv("LINE_CHANNEL_TOKEN"))
+	if err != nil {
+		return nil, err
+	}
+	return &LineBot{api}, nil
+}
+
+func (lineBot *LineBot) BroadcastSimpleMessage(text string) (*map[string]interface{}, error) {
+	return lineBot.api.Broadcast(
 		&messaging_api.BroadcastRequest{
 			Messages: []messaging_api.MessageInterface{
 				messaging_api.TextMessage{
@@ -31,7 +39,7 @@ func HandleLambda() error {
 		return fmt.Errorf("line bot failed in initialization: %s", err)
 	}
 
-	_, err = BroadcastSimpleMessage(lineBot, "Hello World!")
+	_, err = lineBot.BroadcastSimpleMessage("Hello World!")
 	if err != nil {
 		return fmt.Errorf("line bot failed in broadcasting: %s", err)
 	}
