f033e7508de3bc3e98f2e65d8ea75d6fa497d610
sae220 <mod-sharp.truetle3@outlook.jp>
2024-11-22 10:32:21 +0900

    全国地方公共団体コード周りのエラーハンドリングを追加

--- lambda/weather_warnings.go
+++ lambda/weather_warnings.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"errors"
+)
+
 // ある地域に出ている気象警報・注意報一つ
 type AreaWeatherWarning struct {
 	// 気象警報・注意報コード
@@ -13,5 +17,11 @@ type AreaWeatherWarnings []AreaWeatherWarning
 
 // 全国地方公共団体コードに対応する地域に出ている気象警報・注意報を取得する
 func FetchAreaWeatherWarnings(areaCode AreaCode) (AreaWeatherWarnings, error) {
+	if !areaCode.IsValid() {
+		return nil, errors.New("全国地方公共団体コードが無効です")
+	}
+	if areaCode.IsPrefectureAreaCode() {
+		return nil, errors.New("都道府県ではなく市町村のコードを入力してください")
+	}
 	return nil, nil
 }
