1d479214ad701cdfc6e70f187f35427022e319db
sae220 <mod-sharp.truetle3@outlook.jp>
2024-11-22 11:14:40 +0900

    APIのレスポンスに合わせて構造体を作成・変更

--- lambda/weather_warnings.go
+++ lambda/weather_warnings.go
@@ -4,19 +4,35 @@ import (
 	"errors"
 )
 
-// ある地域に出ている気象警報・注意報一つ
-type AreaWeatherWarning struct {
-	// 気象警報・注意報コード
-	code string
-	// 気象警報・注意報の状態
-	status string
-}
+// 気象警報・注意報を取得するAPIのレスポンスの形式
+type (
+	WeatherWarningApiResponse struct {
+		AreaTypes AreaTypes `json:"areaTypes"`
+	}
 
-// ある地域に出ている気象警報・注意報の配列
-type AreaWeatherWarnings []AreaWeatherWarning
+	AreaTypes []AreaType
+
+	AreaType struct {
+		Areas Areas `json:"areas"`
+	}
+
+	Areas []Area
+
+	Area struct {
+		Code     AreaCode `json:"code"`
+		Warnings Warnings `json:"warnings"`
+	}
+
+	Warnings []Warning
+
+	Warning struct {
+		Code   string `json:"code"`
+		Status string `json:"status"`
+	}
+)
 
 // 全国地方公共団体コードに対応する地域に出ている気象警報・注意報を取得する
-func FetchAreaWeatherWarnings(areaCode AreaCode) (AreaWeatherWarnings, error) {
+func FetchAreaWeatherWarnings(areaCode AreaCode) (Warnings, error) {
 	if !areaCode.IsValid() {
 		return nil, errors.New("全国地方公共団体コードが無効です")
 	}
