13a1de20b34055b5c5ed2fd443b550e9b9697f89
sae220 <mod-sharp.truetle3@outlook.jp>
2024-12-02 15:40:49 +0900

    地方公共団体コード周りのテストを追加

--- lambda/jpweatherwarnings/area_code_test.go
+++ lambda/jpweatherwarnings/area_code_test.go
@@ -25,3 +25,37 @@ func TestAreaCode_IsValid(t *testing.T) {
 		})
 	}
 }
+
+func TestAreaCode_IsPrefectureAreaCode(t *testing.T) {
+	tests := map[string]struct {
+		areaCodeText string
+		result       bool
+	}{
+		"正常":  {"060003", true},
+		"市町村": {"062014", false},
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			if got, expected := jpweatherwarnings.AreaCode(test.areaCodeText).IsPrefectureAreaCode(), test.result; got != expected {
+				t.Errorf("AreaCode(%q).IsPrefectureAreaCode() returned %v; expected %v", test.areaCodeText, got, expected)
+			}
+		})
+	}
+}
+
+func TestAreaCode_CodeForAPI(t *testing.T) {
+	areaCode := jpweatherwarnings.AreaCode("062014")
+	expected := "0620100"
+	if got := areaCode.CodeForAPI(); got != expected {
+		t.Errorf("AreaCode(%q).CodeForAPI() returned %v; expected %v", string(areaCode), got, expected)
+	}
+}
+
+func TestAreaCode_PrefectureCodeForAPI(t *testing.T) {
+	areaCode := jpweatherwarnings.AreaCode("062014")
+	expected := "060000"
+	if got := areaCode.CodeForAPI(); got != expected {
+		t.Errorf("AreaCode(%q).PrefectureCodeForAPI() returned %v; expected %v", string(areaCode), got, expected)
+	}
+}
